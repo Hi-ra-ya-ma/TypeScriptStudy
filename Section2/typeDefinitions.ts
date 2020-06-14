723ffc0c794af4a9917f7d22d60c8ab11808896e
hi-ra-ya-ma <hirayama.no.uni@gmail.com>
2020-06-14 14:42:34 +0900

    基本の型定義: string型
    ES6からバッククォートで囲って文字列の内部で変数を展開するテンプレート構文が導入された。
    ${}の部分の名称はプレースホルダー。

--- Section2/typeDefinitions.ts
+++ Section2/typeDefinitions.ts
@@ -4,3 +4,7 @@ let decimal: number = 256; // 10進数
 let hex: number = 0xfff; // 16進数
 let binary: number = 0b0000; // 2進数（ES6で導入）
 let octal: number = 0o123; // 8進数（ES6で導入）
+
+let color: string = "white";
+color = 'black';
+let myColor: string = `my color is ${color}`; // テンプレート構文（ES6で導入）
