6cfc22f1f4877d1d2e719b9bc546edb45fd28825
hi-ra-ya-ma <hirayama.no.uni@gmail.com>
2020-06-14 14:37:51 +0900

    基本の型定義: number型
    JavaScriptと同様、全ての数値は浮動小数点値となる。

--- Section2/typeDefinitions.ts
+++ Section2/typeDefinitions.ts
@@ -1,1 +1,6 @@
 let flag: boolean = false; // boolean型、true, false
+
+let decimal: number = 256; // 10進数
+let hex: number = 0xfff; // 16進数
+let binary: number = 0b0000; // 2進数（ES6で導入）
+let octal: number = 0o123; // 8進数（ES6で導入）
