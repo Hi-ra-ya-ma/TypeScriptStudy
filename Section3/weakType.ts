a85edc06edf9e2aeb5f9119c79ce9c5458a4598e
hi-ra-ya-ma <hirayama.no.uni@gmail.com>
2020-06-17 06:30:05 +0900

    制約による型安全: Weak Type
    全てのプロパティがオプショナルな型のことをWeak Typeと呼ぶ。
    全てのプロパティがオプショナルではあるが、引数は必要という条件のため、23行目、24行目のような判定となる。

---
+++ Section3/weakType.ts
@@ -0,0 +1,24 @@
+type User = {
+  age?: number;
+  name?: string;
+};
+function registerUser(user: User) {}
+
+// 型にはないプロパティを持つオブジェクト
+const maybeUser = {
+  age: 26,
+  name: "Taro",
+  gender: "male",
+};
+
+// 型と一致するプロパティを1つも持たないオブジェクト
+const notUser = {
+  gender: "male",
+  graduate: "Tokyo",
+};
+
+registerUser(maybeUser);
+// registerUser(notUser); // NG
+
+registerUser({});
+// registerUser(); // NG
